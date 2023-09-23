d457ea7f27d0d452b67e74d799f9b7e7f2466f43
Ahammad204 <Ahammadcode@gmail.com>
2023-09-23 22:25:22 +0600

    added phone list

--- src/Components/Phones/PhoneCard.jsx
+++ src/Components/Phones/PhoneCard.jsx
@@ -1,8 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+
+import { Link } from "react-router-dom";
 
 
-const PhoneCard = ({phone}) => {
+const PhoneCard = ({ phone }) => {
 
-    const {id,phone_name,brand_name,rating,price,image} = phone || {}
+    const { id, phone_name, brand_name, rating, price, image } = phone || {}
 
 
     return (
@@ -15,9 +19,9 @@ const PhoneCard = ({phone}) => {
                     />
                 </div>
                 <div className="p-6">
-                <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
-                            {brand_name}
-                        </p>
+                    <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+                        {brand_name}
+                    </p>
                     <div className="mb-2 flex items-center justify-between">
                         <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
                             {phone_name}
@@ -31,12 +35,16 @@ const PhoneCard = ({phone}) => {
                     </p>
                 </div>
                 <div className="p-6 pt-0">
-                    <button
-                        className="block bg-gray-300 w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                        type="button"
-                    >
-                       Favorite
-                    </button>
+                    <Link to={`/phones/${id}`}>
+
+                        <button
+                            className="block bg-gray-300 w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                            type="button"
+                        >
+                            See Details
+                        </button>
+
+                    </Link>
                 </div>
             </div>
         </div>
