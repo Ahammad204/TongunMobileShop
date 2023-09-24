416fd855ae6b5ddf7cf51efcd8a5965339283095
Ahammad204 <Ahammadcode@gmail.com>
2023-09-24 11:25:30 +0600

    setup local storage

--- src/Pages/Fevorites/Fevorites.jsx
+++ src/Pages/Fevorites/Fevorites.jsx
@@ -8,6 +8,7 @@ const Fevorites = () => {
     const [favourites,setFavourites] = useState([]);
     const [noFound,setNoFound] = useState(false)
     const [isShow,setIsShow] = useState(false)
+    const [totalPrice,setTotalPrice] = useState(0)
 
     useEffect(()=>{
 
@@ -15,6 +16,10 @@ const Fevorites = () => {
        if(favoriteItems){
 
         setFavourites(favoriteItems);
+
+        const total = favoriteItems.reduce((preValue,currentItem)=> preValue + currentItem.price, 0)
+        setTotalPrice(total)
+
 
        }
        else{
@@ -39,7 +44,15 @@ const Fevorites = () => {
         <div>
             {noFound ? <p className="h-[80vh] flex justify-center items-center">{noFound}</p>:<div>
 
-                {favourites.length > 0 && <button onClick={handleRemove} className="px-5 bg-green-200 mx-auto block">Delete All Favourites</button>}
+                {favourites.length > 0 && 
+                
+                <div>
+                    <button onClick={handleRemove} className="px-5 bg-green-200 mx-auto block">Delete All Favourites</button>
+                    <h1>TotalPrice: ${totalPrice}</h1>
+                </div>
+                
+                
+                }
                 
                 <div className="grid grid-cols-2 gap-5">
 
